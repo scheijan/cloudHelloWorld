1ce400897366ebc6c3085c8214d9ba8bcb09cadd
scheijan <scheijan@gmail.com>
2018-06-16 01:51:20 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8000
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print("serving at port %s" % PORT)
+httpd.serve_forever()
